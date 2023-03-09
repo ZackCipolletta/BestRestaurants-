cc17a2295c06bb18d8e4942b37e3655c20aaece2
Zachary <ZackCipolletta@gmail.com>
2023-03-09 10:42:46 -0800

    Add star value to each review
    Co-authored-by: Mike Donovan <mike@mikedonovan.dev>
    
    Co-authored-by: James Provance <jb24x7@gmail.com>

--- RestaurantCuisine/Models/Review.cs
+++ RestaurantCuisine/Models/Review.cs
@@ -3,6 +3,7 @@ namespace RestaurantCuisine.Models
   public class Review
   {
     public int ReviewId { get; set; }
+    public int Stars { get; set; }
     public string Title { get; set; }
     public string Body { get; set; }
     public string Author { get; set; }
